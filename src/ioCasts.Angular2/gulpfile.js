3f8d1c4ff3666b47bd861ade3c3d5c793ed7b19c
Thomas Brian <tdbrian@gmail.com>
2015-11-22 11:31:50 -0600

    Initial commit

---
+++ src/ioCasts.Angular2/gulpfile.js
@@ -0,0 +1,59 @@
+﻿var gulp = require("gulp"),
+    rimraf = require("rimraf"),
+    concat = require("gulp-concat"),
+    cssmin = require("gulp-cssmin"),
+    uglify = require("gulp-uglify"),
+    webpack = require('gulp-webpack'),
+    project = require("./project.json");
+
+var paths = {
+    webroot: "./" + project.webroot + "/"
+};
+
+paths.app = "./App/";
+paths.js = paths.webroot + "js/**/*.js";
+paths.minJs = paths.webroot + "js/**/*.min.js";
+paths.css = paths.webroot + "css/**/*.css";
+paths.minCss = paths.webroot + "css/**/*.min.css";
+paths.concatJsDest = paths.webroot + "js/site.min.js";
+paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.ts = paths.app + "**/*.ts";
+
+gulp.task("clean:js", function (cb) {
+    rimraf(paths.concatJsDest, cb);
+});
+
+gulp.task("clean:css", function (cb) {
+    rimraf(paths.concatCssDest, cb);
+});
+
+gulp.task("clean", ["clean:js", "clean:css"]);
+
+gulp.task('pack', function () {
+    gulp.src('./webpack.js')
+      .pipe(webpack({
+          watch: true,
+          module: {
+              loaders: [
+                { test: /\.ts$/, loader: 'style!css' },
+              ],
+          },
+      }))
+      .pipe(gulp.dest('dist/'));
+});
+
+gulp.task("min:js", function () {
+    gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+        .pipe(concat(paths.concatJsDest))
+        .pipe(uglify())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("min:css", function () {
+    gulp.src([paths.css, "!" + paths.minCss])
+        .pipe(concat(paths.concatCssDest))
+        .pipe(cssmin())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("min", ["min:js", "min:css"]);
